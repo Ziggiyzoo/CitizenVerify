c212f28672e7572fd2067d8ac20c0576aa15ec11
David Symons <davidsymons.w@gmail.com>
2024-04-29 01:44:45 +0100

    Attempt to mount secrets

--- AstralAdmin/main.py
+++ AstralAdmin/main.py
@@ -8,6 +8,9 @@ from src.astral_admin import AstralAdmin
 # Main Method
 if __name__ == "__main__":
     # Check for required environment variables
+    print("TOKEN: " + str(environ["TOKEN"]))
+    print("SC API KEY: " + str(environ["SC_API_KEY"]))
+    print("FIREBASE SECRET: " + str(environ["FIREBASE_SECRET"]))
     if "TOKEN" not in environ or environ["TOKEN"] == "":
         raise ValueError(
             "No value for Environment Variable 'TOKEN' supplied. Exiting..."

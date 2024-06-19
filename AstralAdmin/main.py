c2838baeafccce02f210c9b9047b3cc50f5810e0
David Symons <davidsymons.w@gmail.com>
2024-06-19 21:23:19 +0100

    First logging

--- AstralAdmin/main.py
+++ AstralAdmin/main.py
@@ -1,20 +1,26 @@
 """
 Org Discord Bot
 """
+import logging
+
 from os import environ
 
 from src.astral_admin import AstralAdmin
 
+logging.basicConfig(filename="test.log", level=logging.DEBUG,
+                    format="%(asctime)s:%(levelname)s:%(message)s:%(pathname)s:%(lineno)d")
+
 # Main Method
 if __name__ == "__main__":
-    # Check for required environment variables
-    if "TOKEN" not in environ or environ["TOKEN"] == "":
-        raise ValueError(
-            "No value for Environment Variable 'TOKEN' supplied. Exiting..."
-        )
-    print("Running")
-    token: str = environ["TOKEN"]
-    Bot: AstralAdmin = AstralAdmin()
-    Bot.load_extensions("src.cogs", recursive=True)
-    print("Cogs Loaded")
-    Bot.run(token)
+    try:
+        logging.info("Getting Token")
+        token: str = environ["TOKEN"]
+        test_api_key: str = environ["SC_API_KEY"]
+        test_firebase_secret: str = environ["FIREBASE_SECRET"]
+        Bot: AstralAdmin = AstralAdmin()
+        Bot.load_extensions("src.cogs", recursive=True)
+        logging.info("Cogs Loaded, running bot.")
+        Bot.run(token)
+    except KeyError as exc:
+        logging.critical(f"Environment Variable {exc} does not exist. Exiting.")
+          
